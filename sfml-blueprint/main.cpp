28d094dd894066bb182433b0cd1b2d733b687fcf
Sean Bohan <pixelpicosean@gmail.com>
2015-12-27 00:10:41 +0800

    Finish code for chapter one.
    Create a window and draw a simple cyan circle.

--- sfml-blueprint/main.cpp
+++ sfml-blueprint/main.cpp
@@ -1,91 +1,42 @@
-
-//
-// Disclamer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// Note that the "Run Script" build phase will copy the required frameworks
-// or dylibs to your application bundle so you can execute it on any OS X
-// computer.
-//
-// Your resource files (images, sounds, fonts, ...) are also copied to your
-// application bundle. To get the path to these resource, use the helper
-// method resourcePath() from ResourcePath.hpp
-//
-
-#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
 // Here is a small helper for you ! Have a look.
 #include "ResourcePath.hpp"
 
-int main(int, char const**)
-{
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+int main(int argc, char const* argv[]) {
+  // Create the main window
+  sf::RenderWindow window(sf::VideoMode(400, 400), "SFML blueprint");
 
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
-        return EXIT_FAILURE;
-    }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+  // Create a circle
+  sf::CircleShape circle(150);
+  circle.setFillColor(sf::Color::Cyan);
+  circle.setPosition(10, 20);
 
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
+  // Start the game loop
+  while (window.isOpen()) {
+    // Process events
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      // Close window: exit
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
 
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
-        return EXIT_FAILURE;
+      // Escape pressed: exit
+      if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+        window.close();
+      }
     }
 
-    // Play the music
-    music.play();
+    // Clear screen
+    window.clear();
 
-    // Start the game loop
-    while (window.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window: exit
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
+    // Draw something
+    window.draw(circle);
 
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
-            }
-        }
+    // Update the window
+    window.display();
+  }
 
-        // Clear screen
-        window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
-        window.display();
-    }
-
-    return EXIT_SUCCESS;
+  return 0;
 }
