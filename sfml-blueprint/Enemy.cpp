b3a0d6da3b8de56c012a705fff7f2fe2eca89e40
Sean Bohan <pixelpicosean@gmail.com>
2016-01-31 09:28:34 +0800

    Remove Saucer module, move all enemy classes into Enemy module.

--- sfml-blueprint/Enemy.cpp
+++ sfml-blueprint/Enemy.cpp
@@ -8,6 +8,7 @@
 
 #include "Enemy.hpp"
 #include "Scene.hpp"
+#include "Collision.hpp"
 #include "Utils.hpp"
 
 Enemy::Enemy(Data::TEXTURES texID, Scene& scene): Actor(texID, scene) {
@@ -19,4 +20,103 @@ void Enemy::onDestroy() {
   Actor::onDestroy();
 
   Data::addScore(this->getPoints());
+}
+
+// = Saucer ==============================================================
+bool Saucer::isCollide(const Actor &other) const {
+  if (dynamic_cast<const ShootSaucer*>(&other) == nullptr) {
+    return Collision::circleTest(this->_sprite, other._sprite);
+  }
+
+  return false;
+}
+
+void Saucer::update(sf::Time dt) {
+  float seconds = dt.asSeconds();
+
+  Actor *near = nullptr;
+  float nearDistance = 300.0f;
+
+  for (Actor *actorPtr : this->_scene.getActors()) {
+    if (actorPtr != this and (dynamic_cast<const Meteor *>(actorPtr) or (dynamic_cast<const ShootPlayer *>(actorPtr)))) {
+      float x = this->getPosition().x - actorPtr->getPosition().x;
+      float y = this->getPosition().y - actorPtr->getPosition().y;
+      float dist = std::sqrt(x * x + y * y);
+      if (dist < nearDistance) {
+        nearDistance = dist;
+        near = actorPtr;
+      }
+    }
+  }
+
+  if (near != nullptr) {
+    sf::Vector2f pos = near->getPosition();
+    float angleRad = std::atan2(pos.y, pos.x);
+    this->_impulse -= sf::Vector2f(std::cos(angleRad), std::sin(angleRad)) * 300.0f * seconds;
+  }
+  else {
+    sf::Vector2f pos = Data::player->getPosition() - this->getPosition();
+    float angleRad = std::atan2(pos.y, pos.x);
+    this->_impulse += sf::Vector2f(std::cos(angleRad), std::sin(angleRad)) * 300.0f * seconds;
+  }
+
+  this->_sprite.move(seconds * this->_impulse);
+}
+
+void Saucer::onDestroy() {
+  Enemy::onDestroy();
+
+  // TODO: play boom sound
+}
+
+void Saucer::newSaucer(Scene &scene) {
+  Saucer *res = nullptr;
+  if (util::random(0.0f, 1.0f) > Data::getScore() / 4000.0f) {
+    res = new BigSaucer(scene);
+  }
+  else {
+    res = new SmallSaucer(scene);
+  }
+
+  res->setPosition(util::random(0, 1) * scene.getX(), util::random(0.0f, (float)scene.getY()));
+
+  scene.add(res);
+}
+
+// = Small Saucer =========================================================
+SmallSaucer::SmallSaucer(Scene& scene): Saucer(Data::TEXTURES::SMALL_SAUCER, scene) {
+  this->_timeSinceLastShoot = sf::Time::Zero;
+  this->_impulse *= 400.0f;
+
+  // TODO: play shoot sound
+}
+
+int SmallSaucer::getPoints() const {
+  return 200;
+}
+
+void SmallSaucer::update(sf::Time dt) {
+  Saucer::update(dt);
+
+  this->_timeSinceLastShoot += dt;
+  if (this->_timeSinceLastShoot > sf::seconds(1.5)) {
+    if (Data::player != nullptr) {
+      this->_world.add(new ShootSaucer(*this));
+      this->_timeSinceLastShoot = sf::Time::Zero;
+    }
+  }
+}
+
+// = Meteor ==============================================================
+bool Meteor::isCollide(const Enemy &other) const {
+  if (dynamic_cast<const Meteor*>(&other) == nullptr) {
+    return Collision::circleTest(this->_sprite, other._sprite);
+  }
+
+  return false;
+}
+
+void Meteor::update(sf::Time dt) {
+  float seconds = dt.asSeconds();
+  this->_sprite.move(this->_impulse * seconds);
 }
--- sfml-blueprint/Enemy.hpp
+++ sfml-blueprint/Enemy.hpp
@@ -12,6 +12,7 @@
 #include "Actor.hpp"
 #include "Data.hpp"
 
+// Enemy base class
 class Enemy : public Actor {
   public:
     Enemy(const Enemy&) = delete;
@@ -21,6 +22,44 @@ class Enemy : public Actor {
 
     virtual int getPoints() const = 0;
     virtual void onDestroy() override;
+};
+
+
+// Enemies
+class Saucer : public Enemy {
+  public:
+    Saucer(const Saucer&) = delete;
+    Saucer& operator=(const Saucer&) = delete;
+
+    using Enemy::Enemy;
+
+    bool isCollide(const Actor& other) const override;
+    void update(sf::Time dt) override;
+    void onDestroy() override;
+
+    static void newSaucer(Scene& scene);
+};
+
+class SmallSaucer : public Saucer {
+  public:
+    SmallSaucer(Scene& scene);
+
+    virtual int getPoints() const;
+    virtual void update(sf::Time dt);
+
+  private:
+    sf::Time _timeSinceLastShoot;
+};
+
+class Meteor : public Enemy {
+  public:
+    Meteor(const Meteor&) = delete;
+    Meteor& operator=(const Meteor&) = delete;
+
+    using Enemy::Enemy;
+
+    bool isCollide(const Enemy& other) const override;
+    void update(sf::Time dt);
 };
 
 #endif /* Enemy_hpp */
